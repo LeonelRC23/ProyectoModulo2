4b3b489237caa0c29f531e7a59d1b37f04c6cf01
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-12-17 02:28:28 -0300

    se cambiar a boton inhabiliatr

--- JS/usuarios.js
+++ JS/usuarios.js
@@ -1,42 +1,45 @@
+const usuarios = JSON.parse(localStorage.getItem("usuariosKey")) || [];
 
- const usuarios = JSON.parse(localStorage.getItem('usuariosKey')) || [];
+const table = document.querySelector("table tbody");
+usuarios.forEach((usuario) => {
+  const newRow = table.insertRow();
+  newRow.insertCell().textContent = usuario.id;
+  newRow.insertCell().textContent = usuario.nombreCompleto;
+  newRow.insertCell().textContent = usuario.usuario;
+  newRow.insertCell().textContent = usuario.email;
+  newRow.insertCell().textContent = usuario.contraseña;
+  newRow.insertCell().textContent = usuario.rol;
+  newRow.insertCell().textContent = usuario.habilitado;
 
-   const table = document.querySelector('table tbody');
-   usuarios.forEach(usuario => {
-       const newRow = table.insertRow();
-       newRow.insertCell().textContent = usuario.id;
-       newRow.insertCell().textContent = usuario.nombreCompleto;
-       newRow.insertCell().textContent = usuario.usuario;
-       newRow.insertCell().textContent = usuario.email;
-       newRow.insertCell().textContent = usuario.contraseña;
-       newRow.insertCell().textContent = usuario.rol;
-       newRow.insertCell().textContent = usuario.habilitado;
+  const cellAcciones = newRow.insertCell();
 
-       const cellAcciones = newRow.insertCell();
+  const btnGroup = document.createElement("div");
+  btnGroup.classList.add("btn-group");
 
-       const btnGroup = document.createElement('div');
-       btnGroup.classList.add('btn-group');
+  const btnEliminar = document.createElement("button");
+  btnEliminar.textContent = "Eliminar";
+  btnEliminar.classList.add("btn", "btn-danger", "btn-sm");
+  btnEliminar.addEventListener("click", () => eliminarUsuario(usuario.id));
+  btnGroup.appendChild(btnEliminar);
 
-       const btnEliminar = document.createElement('button');
-       btnEliminar.textContent = 'Eliminar';
-       btnEliminar.classList.add('btn', 'btn-danger', 'btn-sm');
-       btnEliminar.addEventListener('click', () => eliminarUsuario(usuario.id));
-       btnGroup.appendChild(btnEliminar);
+  const btnInhabilitar = document.createElement("button");
+  btnInhabilitar.textContent = "Inhabilitar";
+  btnInhabilitar.classList.add("btn", "btn-secondary", "btn-sm");
+  btnInhabilitar.addEventListener("click", () =>
+    inhabilitarUsuario(usuario.id)
+  );
+  btnGroup.appendChild(btnInhabilitar);
 
-       const btnModificar = document.createElement('button');
-       btnModificar.textContent = 'Editar';
-       btnModificar.classList.add('btn', 'btn-warning', 'btn-sm');
-       btnModificar.addEventListener('click', () => modificarUsuario(usuario.id));
-       btnGroup.appendChild(btnModificar);
+  cellAcciones.appendChild(btnGroup);
+});
 
-       cellAcciones.appendChild(btnGroup);
-   });
-
-   const eliminarUsuario = (id) => {
-    const confirmacion = confirm('¿Estás seguro de que quieres eliminar este usuario?');
-    if (confirmacion) {
-        const nuevosUsuarios = usuarios.filter(usuario => usuario.id !== id);
-        localStorage.setItem('usuariosKey', JSON.stringify(nuevosUsuarios));
-        window.location.reload();
-    }
+const eliminarUsuario = (id) => {
+  const confirmacion = confirm(
+    "¿Estás seguro de que quieres eliminar este usuario?"
+  );
+  if (confirmacion) {
+    const nuevosUsuarios = usuarios.filter((usuario) => usuario.id !== id);
+    localStorage.setItem("usuariosKey", JSON.stringify(nuevosUsuarios));
+    window.location.reload();
+  }
 };
